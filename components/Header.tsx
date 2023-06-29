ea735d62f095fa01c0abed17163761fde5434532
Carmel Isaac <96634269+CarmelIsaac@users.noreply.github.com>
2023-06-29 18:18:56 +0300

    final

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,6 +12,7 @@ import { toast } from "react-hot-toast";
 
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
+import usePlayer from "@/hooks/usePlayer";
 
 import Button from "./Button";
 
@@ -21,6 +22,7 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const player = usePlayer();
   const authModal = useAuthModal();
   const router = useRouter();
 
@@ -30,7 +32,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
   const handleLogout = async () => {
     // Handle Logout
     const { error } = await supabaseClient.auth.signOut();
-    // TODO: Reset any playing songs
+    player.reset();
     router.refresh();
 
     if (error) {
